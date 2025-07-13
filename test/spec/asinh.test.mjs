7789be0aa15404c211f83473064e68a1594a24ba
kato takeshi <tkskto@gmail.com>
2025-07-13 18:40:20 +0900

    Implement asinh function with tests and docs

--- test/spec/asinh.test.mjs
+++ test/spec/asinh.test.mjs
@@ -1,1 +1,12 @@
-// TODO
+import test from 'ava';
+import {asinh} from '../built-in-functions/asinh.mjs';
+
+test('asinh with simple number', (t) => {
+    t.is(asinh(0), 0);
+    t.is(asinh(1), 0.881373587019543);
+    t.is(asinh(-1), -0.881373587019543);
+    t.is(asinh(10), 2.99822295029797);
+    t.is(asinh(-10), -2.99822295029797);
+});
+
+// vec2以下のテストは省略する。
