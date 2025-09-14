7a25fa16131959bf63eb49bbb8716a235d2e1a2c
kato takeshi <tkskto@gmail.com>
2025-09-14 16:09:19 +0900

    docs: fix distance references

--- test/spec/distance.test.mjs
+++ test/spec/distance.test.mjs
@@ -1,1 +1,15 @@
-// TODO
+import test from 'ava';
+import {distance} from '../built-in-functions/distance.mjs';
+import {vec2} from '../basic-types/vec2.mjs';
+
+test('distance with simple number', (t) => {
+    t.is(distance(0, 0), 0);
+    t.is(distance(0, 1), 1);
+    t.is(distance(-1, 1), 2);
+});
+
+test('distance with vec2', (t) => {
+    const a = vec2(0, 0);
+    const b = vec2(1, 1);
+    t.is(distance(a, b), Math.sqrt(2));
+});
